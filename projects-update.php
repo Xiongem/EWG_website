f7ab3088ae89fd0ab7d04e2f5b15e82987f5ce9a
Jamie <earthlove97@gmail.com>
2025-03-14 15:47:03 +0900

    project update no date

--- projects-update.php
+++ projects-update.php
@@ -200,7 +200,7 @@ switch ($genre) {
 
         if (checkBox.checked == true) {
             document.getElementById("goal_date").disabled = true;
-            document.getElementById("goal_date").value = 0;
+            document.getElementById("goal_date").value = 0000-00-00;
         } else if (checkBox.checked == false) {
             document.getElementById("goal_date").disabled = false;
         }
