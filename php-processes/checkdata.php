d65c453a04a7092b0f2d20981f3281d08d6354c5
Jamie <earthlove97@gmail.com>
2025-03-14 12:06:49 +0900

    adding username checking to signup process

---
+++ php-processes/checkdata.php
@@ -0,0 +1,22 @@
+<?php
+ob_start();
+
+session_start();
+require($_SERVER['DOCUMENT_ROOT'] . '/php-processes/utilities.php');
+dbConnect();
+
+if (! empty($_POST["username"])) {
+    $sql = "SELECT * FROM users WHERE username=?";
+        $statement = $_SESSION["conn"]->prepare($sql);
+        $statement->bind_param('s', 
+                    $_POST["username"]);
+        $statement->execute();
+        $result = $statement->get_result();
+    if ($result->num_rows > 0) {
+        echo "User Name Already Exist";
+    } else {
+        echo "Username Available";
+    }
+ exit();
+}
+?>
