6791b01f47c52092bdc66127b411f558a3680109
Jamie <earthlove97@gmail.com>
2025-03-14 13:57:47 +0900

    new popup

--- php-processes/utilities.php
+++ php-processes/utilities.php
@@ -117,7 +117,8 @@ function makePopup() {
             <div class="container">
             <div class="not-logged-in" id="not-logged-in">
                 <p>Psst, hey you. If you already have an account just click the Login link above. 
-                    If you're new click <a href="account-create.html">here</a> to sign up and 
+                <br>    
+                If you're new click <a href="account-create.html">here</a> to sign up and 
                     start earning badges!</p>
             </div>
         HTML;
