12616a6e477ce5c4908a18276818020d31f369eb
Jamie <earthlove97@gmail.com>
2025-03-16 02:15:32 +0900

    fix dropdown glitch

--- javascript/script.js
+++ javascript/script.js
@@ -101,6 +101,7 @@ function hideWarning() {
             //PROFILE ICON
             function myFunction() {
               document.getElementById("myDropdown").classList.toggle("show");
+              console.log("first success");
           }
           // Close the dropdown menu if the user clicks outside of it
           // window.onclick = function(event2) {
@@ -125,25 +126,26 @@ function hideWarning() {
         }
         // Close the dropdown menu if the user clicks outside of it
         window.onclick = function(event, events) {
-            // if (!event.target.matches('.dropbtn2')) {
-            //     var dropdowns2 = document.getElementsByClassName("dropdown-content2");
-            //     var i;
-            //     for (i = 0; i < dropdowns2.length; i++) {
-            //         var openDropdown2 = dropdowns2[i];
-            //         if (openDropdown2.classList.contains('show2')) {
-            //         openDropdown2.classList.remove('show2');
-            //         }
-            //     }
-            // }
+          if (!event.target.matches('.dropbtn2')) {
+            var dropdowns2 = document.getElementsByClassName("dropdown-content2");
+            var i;
+            for (i = 0; i < dropdowns2.length; i++) {
+              var openDropdown2 = dropdowns2[i];
+              if (openDropdown2.classList.contains('show2')) {
+                openDropdown2.classList.remove('show2');
+                console.log("second success close");
+              }
+            }
+          }
           if (!events.target.matches('.dropbtn')) {
             var dropdowns = document.getElementsByClassName("dropdown-content");
             var i;
             for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-                console.log("first success");
-            }
+              var openDropdown = dropdowns[i];
+              if (openDropdown.classList.contains('show')) {
+                  openDropdown.classList.remove('show');
+                  console.log("first success close");
+              }
             }
           }
         }
