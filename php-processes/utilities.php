9a711370906de756da83de53bdb0943d46e24eb6
Jamie <earthlove97@gmail.com>
2025-03-16 00:58:34 +0900

    fix dropdown glitch

--- php-processes/utilities.php
+++ php-processes/utilities.php
@@ -36,26 +36,7 @@ function makeDropDown() {
             </div>
         </div>
         <script>
-            /* When the user clicks on the button,
-            toggle between hiding and showing the dropdown content */
-            //PROFILE ICON
-            function myFunction() {
-                document.getElementById("myDropdown").classList.toggle("show");
-            }
-            // Close the dropdown menu if the user clicks outside of it
-            window.onclick = function(event2) {
-            if (!event2.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                var i;
-                for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                    console.log("first success");
-                }
-                }
-            }
-            } 
+            
         </script>
     HTML;
         echo $htmlContent;
