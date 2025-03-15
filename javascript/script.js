0095104d7b0bb594c6f130259a64d40dd6e77854
Jamie <earthlove97@gmail.com>
2025-03-16 00:44:37 +0900

    fix dropdown glitch

--- javascript/script.js
+++ javascript/script.js
@@ -26,59 +26,6 @@ window.onload = function getTheme() {
       showPass.type = "password";
     }
   } 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-//PROFILE ICON
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-// Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-//     var dropdowns = document.getElementsByClassName("dropdown-content");
-//     var i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//         console.log("first success");
-//       }
-//     }
-//   }
-// } 
-
-// Close the dropdown menu if the user clicks outside of it
-// window.onclick = function(event) {
-// if (!event.target.matches('.dropbtn2')||!event.target.matches('.dropbtn')) {
-//   var dropdowns = document.getElementsByClassName("dropdown-content");
-//   var dropdowns2 = document.getElementsByClassName("dropdown-content2");
-//   var i;
-//   var a;
-//   for (i = 0; i < dropdowns2.length; i++) {
-//     var openDropdown2 = dropdowns2[i];
-//     if (openDropdown2.classList.contains('show2')) {
-//       openDropdown2.classList.remove('show2');
-//     }
-//   }
-//   for (a = 0; a < dropdowns.length; a++) {
-//     var openDropdown = dropdowns[a];
-//     if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//         console.log("first success");
-//     }
-//   }
-// }
-// }
-
-document.addEventListener('click', function(event) {
-  const dropdowns = document.querySelectorAll('.dropdown'); // Select all dropdown elements
-
-  dropdowns.forEach(function(dropdown) {
-    if (!dropdown.contains(event.target)) { // If the click is outside the dropdown
-      dropdown.classList.remove('show'); // Close the dropdown
-    }
-  });
-});
 //~~~~~~~~~~~~~~~~~~~~~~~~~//
 //#### Button Actions #####//
 //~~~~~~~~~~~~~~~~~~~~~~~~~//
