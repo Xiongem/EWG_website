b3a20f1e47218c1973e92c1c203b6973a68f219e
Jamie <earthlove97@gmail.com>
2025-03-16 00:47:07 +0900

    fix dropdown glitch

--- php-processes/utilities.php
+++ php-processes/utilities.php
@@ -35,6 +35,28 @@ function makeDropDown() {
                     </div>
             </div>
         </div>
+        <script>
+            /* When the user clicks on the button,
+            toggle between hiding and showing the dropdown content */
+            //PROFILE ICON
+            function myFunction() {
+                document.getElementById("myDropdown").classList.toggle("show");
+            }
+            // Close the dropdown menu if the user clicks outside of it
+            window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                var dropdowns = document.getElementsByClassName("dropdown-content");
+                var i;
+                for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                    console.log("first success");
+                }
+                }
+            }
+            } 
+        </script>
     HTML;
         echo $htmlContent;
 } elseif(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true && $_SESSION["overlay"] == "images/badges/hydra-slayer-mono.png"){
