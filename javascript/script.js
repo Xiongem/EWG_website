287e3eb06dd3a20a152fdb0718572977ceef6732
Jamie <earthlove97@gmail.com>
2025-03-16 01:01:07 +0900

    fix dropdown glitch

--- javascript/script.js
+++ javascript/script.js
@@ -99,23 +99,40 @@ function hideWarning() {
 /* When the user clicks on the button,
             toggle between hiding and showing the dropdown content */
             //PROFILE ICON
-            function myFunction() {
-              document.getElementById("myDropdown").classList.toggle("show");
-          }
-          // Close the dropdown menu if the user clicks outside of it
-          window.onclick = function(event2) {
-          if (!event2.target.matches('.dropbtn')) {
-              var dropdowns = document.getElementsByClassName("dropdown-content");
-              var i;
-              for (i = 0; i < dropdowns.length; i++) {
-              var openDropdown = dropdowns[i];
-              if (openDropdown.classList.contains('show')) {
-                  openDropdown.classList.remove('show');
-                  console.log("first success");
-              }
-              }
-          }
-          } 
+          //   function myFunction() {
+          //     document.getElementById("myDropdown").classList.toggle("show");
+          // }
+          // // Close the dropdown menu if the user clicks outside of it
+          // window.onclick = function(event2) {
+          // if (!event2.target.matches('.dropbtn')) {
+          //     var dropdowns = document.getElementsByClassName("dropdown-content");
+          //     var i;
+          //     for (i = 0; i < dropdowns.length; i++) {
+          //     var openDropdown = dropdowns[i];
+          //     if (openDropdown.classList.contains('show')) {
+          //         openDropdown.classList.remove('show');
+          //         console.log("first success");
+          //     }
+          //     }
+          // }
+          // } 
 
 
-          
+           //PROJECTS DROPDOWN
+        //    function myFunction2() {
+        //     document.getElementById("myDropdown2").classList.toggle("show2");
+        //     console.log("second success");
+        // }
+        // // Close the dropdown menu if the user clicks outside of it
+        // window.onclick = function(event) {
+        //     if (!event.target.matches('.dropbtn2')) {
+        //         var dropdowns2 = document.getElementsByClassName("dropdown-content2");
+        //         var i;
+        //         for (i = 0; i < dropdowns2.length; i++) {
+        //             var openDropdown2 = dropdowns2[i];
+        //             if (openDropdown2.classList.contains('show2')) {
+        //             openDropdown2.classList.remove('show2');
+        //             }
+        //         }
+        //     }
+        // }
