b03502b76adf9e229d1cf1290c115f93b611fceb
Jamie <earthlove97@gmail.com>
2025-03-17 09:33:52 +0900

    fix dropdown close glitch

--- index.php
+++ index.php
@@ -375,32 +375,18 @@ $_SESSION["daily"] = $daily;
     </div>
     <?php makeFooter() ?>
     <script>
-        // window.onclick = function(event) {
-        //   if (!event.target.matches('.dropbtn2')) {
-        //     var dropdowns2 = document.getElementsByClassName("dropdown-content2");
-        //     var i;
-        //     for (i = 0; i < dropdowns2.length; i++) {
-        //       var openDropdown2 = dropdowns2[i];
-        //       if (openDropdown2.classList.contains('show2')) {
-        //         openDropdown2.classList.remove('show2');
-        //         console.log("second success close");
-        //       }
-        //     }
-        //   }
-        
-        //   else if (!event.target.matches('.dropbtn')) {
-        //     var dropdowns = document.getElementsByClassName("dropdown-contents");
-        //     var a;
-        //     for (a = 0; a < dropdowns.length; a++) {
-        //       var openDropdown = dropdowns[a];
-        //       if (openDropdown.classList.contains('show')) {
-        //           openDropdown.classList.remove('show');
-        //           console.log("first success close");
-        //       }
-        //     }
-        //   }
-        // }
-
+window.onclick = function(event) {
+    var dropdowns = document.getElementsByClassName("dropdown-contents"); // Replace "dropdown-content" with the appropriate class name for your dropdowns
+    for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) { // Replace 'show' with the class name you use to show the dropdown
+            if (!openDropdown.contains(event.target)) { // Check if the click was inside the dropdown
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     var days = "<?=$days?>";
     if (days == 0) {
         document.getElementById('daysLeft').style.display = 'none';
