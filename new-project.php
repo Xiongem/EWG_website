770944e2ff9efee82f83427df2568368da36b089
Jamie <earthlove97@gmail.com>
2025-03-18 09:00:14 +0900

    ajax to show recommended daily goal

--- new-project.php
+++ new-project.php
@@ -23,6 +23,7 @@ dbConnect();
     <script src="javascript/script.js"></script>
     <script src="javascript/dropDown.js"></script>
     <script src="javascript/ajax.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 <body>
     <header>
