fdeb42e9af97ec938f3481cf254fd8938a521c32
Jamie <earthlove97@gmail.com>
2025-03-31 14:04:20 +0900

    fix mailer issue

--- mailer.php
+++ mailer.php
@@ -8,7 +8,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 require "vendor/autoload.php";
-require($_SERVER['DOCUMENT_ROOT'] . '/PHPMailer/src/PHPMailer.php');
+require($_SERVER['DOCUMENT_ROOT'] . '/phpmailer/src/phpmailer.php');
 
 // $mail = new PHPMailer(true);
 $mail = new PHPMailer\PHPMailer\PHPMailer(); 
