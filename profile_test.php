b0a6871e290b36d280fe6734df9f4ce73950006e
Jamie <earthlove97@gmail.com>
2025-04-02 10:01:53 +0900

    test

--- profile_test.php
+++ profile_test.php
@@ -11,7 +11,7 @@ dbConnect();
 //assigns the user's id for all the sql
 // $userID = htmlspecialchars($_SESSION["user_id"]);
 $userNAME = $_GET["userNAME"];
-echo $username;
+echo $userNAMEe;
 //queries the user's profile data
 $sql = "SELECT * FROM users WHERE username=$userNAME";
 $result = $_SESSION["conn"]->query($sql);
@@ -29,7 +29,7 @@ if (empty($pfp)) {
     $pfp_set = $pfp;
 }
 //echos project info if there is any or default values if not
-$sql = "SELECT * FROM current_project WHERE username=$username AND current_state='current'";
+$sql = "SELECT * FROM current_project WHERE username=$userNAME AND current_state='current'";
 $result = $_SESSION["conn"]->query($sql);
 $user = $result->fetch_assoc();
     $genre = $user["genre"];
@@ -38,7 +38,7 @@ $user = $result->fetch_assoc();
     $default = "images/genre-covers/placeholder.jpg";
 
 if (isset($user["genre"])){
-    $sql = "SELECT created_at FROM current_project WHERE username=$username";
+    $sql = "SELECT created_at FROM current_project WHERE username=$userNAME";
     $result = $_SESSION["conn"]->query($sql);
     $dates = $result->fetch_assoc();
         
@@ -70,7 +70,7 @@ $sql = "SELECT `start-1st-project`, `first-daily`, `quarter-quomplete`, `half-wa
 `cross-finish`, `on-track`, `out-gate`, `streak-two`, `streak-three`, `streak-seven`, 
 `streak-fourteen`, `streak-twentyOne`, `every-streak`, `back-it-up`, `outline`, `journey`, 
 `dual-wielder`, `gathering`, `hear-ye`, `breakthrough`, `starting-fresh`, `ever-persist`, 
-`touch-grass`, `business`, `tears-wept`, `overachiever`, `finish-him` FROM current_project WHERE username=$username AND current_state='current'";
+`touch-grass`, `business`, `tears-wept`, `overachiever`, `finish-him` FROM current_project WHERE username=$userNAME AND current_state='current'";
     $result = $_SESSION["conn"]->query($sql);
     $badge = $result->fetch_assoc();
         $badge1 = $badge["start-1st-project"];
@@ -132,7 +132,7 @@ $sql = "SELECT `start-1st-project`, `first-daily`, `quarter-quomplete`, `half-wa
             
 $sql = "SELECT `complete-one-project`, `complete-five-project`, `complete-ten-project`, `streak-fiend`, 
 `hydra-slayer`, `vet-streaker`, `vet-guild`, `start-first-project`, `spicy-spicy`, 
-`tears-alltime` FROM users WHERE id=$userID";
+`tears-alltime` FROM users WHERE username=$userNAME";
     $result = $_SESSION["conn"]->query($sql);
     $badge = $result->fetch_assoc();
         $badge29 = $badge["complete-one-project"];
