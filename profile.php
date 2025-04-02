303ad5094b2cc72230cdba109e6ae3e048dafdfa
Jamie <earthlove97@gmail.com>
2025-04-02 13:58:54 +0900

    update logic

--- profile.php
+++ profile.php
@@ -676,10 +676,9 @@ $_SESSION["overlay"] = $badge["hydra-slayer"];
             <h3 style="text-align: center;">Click on a badge to award it to yourself! Click on it again to remove it.</h3>
     <?php makeFooter() ?>
         <script>
-        
+        var getUserID = <?= $userID ?>;
+        var sessionUserID = <?= $_SESSION["user_id"] ?>;
         if (typeof <?= $_SESSION["user_id"] ?> !== 'undefined' && myVar !== null) {
-            var getUserID = <?= $userID ?>;
-            var sessionUserID = <?= $_SESSION["user_id"] ?>;
                 if (getUserID != sessionUserID ||empty(sessionUserID)) {
                     document.getElementById('add-new-container').style.display = 'none';
                 }
