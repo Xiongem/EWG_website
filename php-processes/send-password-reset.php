c54271d75571e7cd12dac7c736d5cbd1b5d23a66
Jamie <earthlove97@gmail.com>
2025-04-03 13:46:08 +0900

    testing reset validation

--- php-processes/send-password-reset.php
+++ php-processes/send-password-reset.php
@@ -48,3 +48,4 @@ if($_SESSION["conn"]->affected_rows) {
 }
 
 echo "Message sent, please check your inbox.";
+header("Location: /");
--- reset-password.php
+++ reset-password.php
@@ -1,4 +1,28 @@
-<!DOCTYPE html>
+<?php
+$token = $_GET["token"];
+$token_hash = hash("sha256", $token);
+
+ob_start();
+dbConnect();
+
+$sql = "SELECT * FROM users WHERE reset_token_hash = ?";
+$stmt = $_SESSION["conn"] -> prepare($sql);
+$stmt->bind_param("s", $token_hash);
+$stmt -> execute() ;
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+
+if ($user === null) {
+    die("token not found");
+}
+
+if (strtotime($user["reset_token_expires_at"]) <= time()) {
+    die("token has expired");
+}
+
+echo "token has not expired"
+?>
+<!-- <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8" />
@@ -96,4 +120,4 @@
         </div>
     </div>
 </body>
-</html>
+</html> -->
