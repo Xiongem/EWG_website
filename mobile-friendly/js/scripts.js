1ea2ee06927d04c96836f34bce4c962fac6a7345
Jamie <earthlove97@gmail.com>
2025-05-19 10:31:41 +0900

    more work on new project and took EWG out of title head

--- mobile-friendly/js/scripts.js
+++ mobile-friendly/js/scripts.js
@@ -2,3 +2,27 @@
 function navMenuClick() {
     document.getElementById('navMenu').classList.toggle('show');     
 }
+
+function findDailyGoal(){
+    var goal=document.getElementById( "goal" ).value;
+    var goalDate=document.getElementById( "goal_date" ).value;
+    
+    if(goalDate){
+        $.ajax({
+        type: 'post',
+        url: 'php-processes/math',
+        data: {
+            goal: goal,
+            goalDate: goalDate,
+        },
+        success: function (data) {
+            $( '#recommend' ).html(data);
+        }
+        });
+    }
+    else{
+        $( '#recommend' ).html("");
+        console.log("Something went wrong")
+        return false;
+    }
+}
