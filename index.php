d1b76d681acab192b1c062ec046bc7ebd0353099
Jamie <earthlove97@gmail.com>
2025-09-28 11:41:03 +0900

    added datbase and utilities connection

--- index.php
+++ index.php
@@ -3,6 +3,11 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('log_errors', 'On');
 ini_set('error_log', '/path/to/php_errors.log');
+
+
+ob_start();
+require($_SERVER['DOCUMENT_ROOT'] . '/php-processes/utilities.php');
+dbConnect();
 ?>
 
 <!DOCTYPE html>
