538d2d5a2e39c629c13388190eaea75d6ad9912a
Jamie-Work <earthlove97@gmail.com>
2025-09-29 12:23:37 +0900

    test

--- index.php
+++ index.php
@@ -150,8 +150,8 @@ $_SESSION["username"] = $username;
                                     }
                                 }
                         ?>
-            <div class="project-select-content" onclick="projectSelect(project)">
-                <script>var project = "<?= $title ?>";</script>
+            <div class="project-select-content" onclick="projectSelect('<?= $title ?>')">
+                
                 <img class="popup-image" src=<?= $genre_picture ?> alt="genre cover image">
                 <div class="project-info">
                     <h3 id="popup-project-title"><i class="fa fa-star" id="star-icon" alt="star icon"></i> 
