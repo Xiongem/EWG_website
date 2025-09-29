ef7e402a625f8816f7ae9cc454a4d77bfcfe1b68
Jamie-Work <earthlove97@gmail.com>
2025-09-29 11:59:16 +0900

    test

--- js/scripts.js
+++ js/scripts.js
@@ -103,7 +103,7 @@ function projectSelect(title) {
   //     id: id,
   //     badge: badge
   // });
-  var hatred = title;
+  var hatred = title + 5;
   console.log(hatred);
   var body = document.getElementById("body");
   document.getElementById("project-popup").style.display = "none";
