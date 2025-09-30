6fb4662b29c7350031adec34bb5b0708f04e6b08
Jamie-Work <earthlove97@gmail.com>
2025-09-30 09:33:05 +0900

    moved the query selector down below the post request

--- js/activeProject.js
+++ js/activeProject.js
@@ -1,12 +1,12 @@
 function projectSelect(id, display) {
   //assign values
   var project = id;
-  var notActive = document.querySelectorAll("[id='inactive']");
   //begin post method
   $.post("php-processes/update-activeProject", {
       //DATA
       project: project
   });
+  var notActive = document.querySelectorAll("[id='inactive']");
   for (let i = 0; i < notActive.length; i++) {
     if (!notActive[i].classList.contains("hide")) {
       notActive.classList.add("hide");
