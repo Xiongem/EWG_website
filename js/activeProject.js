cbca1f81a66711f1e90b81feb964e91186a0fa40
Jamie-Work <earthlove97@gmail.com>
2025-09-30 09:49:25 +0900

    moved everything above post bc no console log

--- js/activeProject.js
+++ js/activeProject.js
@@ -1,15 +1,16 @@
 function projectSelect(id, display) {
   //assign values
   var project = id;
+  console.log("sent post");
+  var i = document.getElementById(project);
+  i.classList.remove("inactive");
+  console.log("removed class");
   //begin post method
   $.post("php-processes/update-activeProject", {
       //DATA
       project: project
   });
-  console.log("sent post");
-  var i = document.getElementById(project);
-  i.classList.remove("inactive");
-  console.log("removed class");
+  
   // var notActive = document.querySelectorAll("[id='inactive']");
   // for (let i = 0; i < notActive.length; i++) {
   //   if (!notActive[i].classList.contains("hide")) {
