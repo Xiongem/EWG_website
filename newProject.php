99b763b58540afa8d2404577a530f834575bf0a5
Jamie-Work <earthlove97@gmail.com>
2025-10-01 11:27:15 +0900

    test

--- newProject.php
+++ newProject.php
@@ -35,10 +35,10 @@ $userID = $_SESSION["user_id"];
         <?php makeNav() ?>
     </header>
     <?php
-        $sql = "SELECT current_state FROM current_project WHERE users_id=$userID AND current_state='current'";
-        $result = $_SESSION["conn"]->query($sql);
-        if ($result->num_rows == 5) { 
-        ?>
+    $sql = "SELECT current_state FROM current_project WHERE users_id=$userID AND current_state='current'";
+    $result = $_SESSION["conn"]->query($sql);
+    if ($result->num_rows == 5) { 
+    ?>
 
         <div class="archive-warning">
             <h3>You already have a maximum of 5 projects in progress. Click 
