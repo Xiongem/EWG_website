411df168677f0cc49e33e9957372c01e8e6a0496
Jamie-Work <earthlove97@gmail.com>
2025-10-01 11:32:26 +0900

    test

--- newProject.php
+++ newProject.php
@@ -39,7 +39,8 @@ $sql = "SELECT current_state FROM current_project WHERE users_id=$userID AND cur
     <header>
         <?php makeNav() ?>
     </header>
-    <?php if ($result->num_rows == 5) { ?>
+    <?php 
+    if ($result->num_rows == 5) { ?>
     <div class="archive-warning hide">
         <h3>You already have a maximum of 5 projects in progress. Click 
             <a href="archives.php">here</a> to archive one.</h3>
