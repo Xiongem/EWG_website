edad06291cc063a7a5f1009762c3989ae3f67a42
Jamie-Work <earthlove97@gmail.com>
2025-10-01 11:25:43 +0900

    test

--- newProject.php
+++ newProject.php
@@ -37,13 +37,14 @@ $userID = $_SESSION["user_id"];
     <?php
         $sql = "SELECT current_state FROM current_project WHERE users_id=$userID AND current_state='current'";
         $result = $_SESSION["conn"]->query($sql);
-        if ($result->num_rows == 5) { ?>
+        if ($result->num_rows == 5) { 
+        ?>
 
         <div class="archive-warning">
             <h3>You already have a maximum of 5 projects in progress. Click 
                 <a href="archives.php">here</a> to archive one.</h3>
         </div>
-        
+
     <?php}?>
     <div class="new-project-wrapper">
         <h1>Create a New Project</h1>
