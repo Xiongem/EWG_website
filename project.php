a3d604bbddde3528e04c9d64ebe913a776abd048
Jamie-Work <earthlove97@gmail.com>
2025-10-10 15:50:31 +0900

    fixed typo

--- project.php
+++ project.php
@@ -9,7 +9,7 @@ ob_start();
 require($_SERVER['DOCUMENT_ROOT'] . '/php-processes/utilities.php');
 dbConnect();
 $userID = htmlspecialchars($_SESSION["user_id"]);
-$projectID = $_GET["project"];
+$projectID = $_GET["projectID"];
 
 $sql = "SELECT * FROM current_project WHERE id='$projectID'";
 $result = $_SESSION["conn"]->query($sql);
