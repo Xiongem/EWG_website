6c6994d3223b74b54ec2d0491361157ae6f89282
Jamie-Work <earthlove97@gmail.com>
2025-10-14 12:57:25 +0900

    removed preview js bc unneeded

--- js/preview.js
+++
@@ -1,8 +0,0 @@
-function projectSelect(i) {
-    $("#title").keyup(function(){
-        // Getting the current value of textarea
-        var currentText = $(this).val();		
-         // Setting the Div content
-        $("#preview-title").text(currentText);
-    });
-}
