2325686c1bf91b101276736f7e81db219f88ff13
Jamie <earthlove97@gmail.com>
2025-10-16 18:10:22 +0900

    test

--- index.php
+++ index.php
@@ -111,7 +111,7 @@ if (isset($_SESSION["user_id"])) {
         if ($intervals <= 1) {
             if ($streak >= 2) {
                 $badge5 = "images/badges/streak-two-color.webp";
-                echo $streak;
+                echo $displayTitle;
             }else {
                 $badge5 = "images/badges/streak-two-mono.webp";
             }
