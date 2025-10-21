26d35b0b6bf22a33dfcdc0ddcc8d684f4c2de41e
Jamie-Work <earthlove97@gmail.com>
2025-10-21 20:34:32 +0900

    added php sql file to change current state to finished on button click

---
+++ php-processes/completeProject.php
@@ -0,0 +1,21 @@
+<?php
+ob_start();
+session_start();
+require($_SERVER['DOCUMENT_ROOT'] . '/php-processes/utilities.php');
+dbConnect();
+
+$projectID = $_GET["projectID"];
+$userID = $_SESSION["user_id"];
+$completed = "completed";
+
+$stmt = $_SESSION["conn"] -> prepare("UPDATE current_project SET current_state=? WHERE users_id=$userID AND current_state='current' AND id=$projectID");
+$stmt->bind_param("s",
+                        $completed);
+    echo "stmt prepared and bound!".'<br>';
+
+if ($stmt -> execute()) {
+    header("Location: /project.php?projectID=$projectID");
+    exit;
+} else {
+    die("an unexpected error occured");
+}
