86b2f1657604c4b301227041ef471758505b00d6
Jamie-Work <earthlove97@gmail.com>
2025-10-21 20:56:09 +0900

    revert back bc file path is working now????

--- php-processes/completeProject.php
+++ php-processes/completeProject.php
@@ -7,15 +7,15 @@ dbConnect();
 $projectID = $_GET["projectID"];
 $userID = $_SESSION["user_id"];
 $completed = "completed";
-echo $projectID;
-// $stmt = $_SESSION["conn"] -> prepare("UPDATE current_project SET current_state=? WHERE users_id=$userID AND current_state='current' AND id=$projectID");
-// $stmt->bind_param("s",
-//                         $completed);
-//     echo "stmt prepared and bound!".'<br>';
 
-// if ($stmt -> execute()) {
-//     header("Location: /archives.php");
-//     exit;
-// } else {
-//     die("an unexpected error occured");
-// }
+$stmt = $_SESSION["conn"] -> prepare("UPDATE current_project SET current_state=? WHERE users_id=$userID AND current_state='current' AND id=$projectID");
+$stmt->bind_param("s",
+                        $completed);
+    echo "stmt prepared and bound!".'<br>';
+
+if ($stmt -> execute()) {
+    header("Location: /project.php?projectID=$projectID");
+    exit;
+} else {
+    die("an unexpected error occured");
+}
