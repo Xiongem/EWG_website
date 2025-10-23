3a4ded5b530f7b39db9d985e97b1fbaefe332c20
Jamie-Work <earthlove97@gmail.com>
2025-10-23 16:12:08 +0900

    error reporting off

--- php-processes/signup.php
+++ php-processes/signup.php
@@ -1,8 +1,8 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('log_errors', 'On');
-ini_set('error_log', '/path/to/php_errors.log');
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('log_errors', 'On');
+// ini_set('error_log', '/path/to/php_errors.log');
 
 ob_start();
 
