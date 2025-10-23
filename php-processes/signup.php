65fcea609bbdfabf52f55bc09b0b25c8a9f5c687
Jamie-Work <earthlove97@gmail.com>
2025-10-23 16:17:03 +0900

    test

--- php-processes/signup.php
+++ php-processes/signup.php
@@ -33,34 +33,3 @@ if ($_POST["pwd"] !== $_POST["confirm_pwd"]) {
     die("Passwords must match");
 }
 
-$password_hash = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
-
-$stmt = $_SESSION["conn"] -> prepare("INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)");
-$stmt->bind_param("sss",
-                    $_POST["username"],
-                    $_POST["email"],
-                    $password_hash);
-
-if ($stmt -> execute()) {
-    $sql = sprintf("SELECT * FROM users
-                    WHERE username = '%s'",
-                    $_SESSION["conn"]->real_escape_string($_POST["username"]));
-
-    $result = $_SESSION["conn"]->query($sql);
-
-    $user = $result->fetch_assoc();
-
-        $_SESSION['loggedin'] = true;
-        $_SESSION["user_id"] = $user["id"];
-
-        header("Location: /profile-create.html");
-        exit;
-    } else {
-        die("something went wrong");
-}
-
-
-
-$stmt -> close();
-mysqli_close($conn);
-?>
