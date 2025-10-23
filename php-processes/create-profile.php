439924a2386218f3ed4fbe6455b685fa70a41ec8
Jamie-Work <earthlove97@gmail.com>
2025-10-23 18:35:21 +0900

    added php profile creation

---
+++ php-processes/create-profile.php
@@ -0,0 +1,32 @@
+<?php
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('log_errors', 'On');
+// ini_set('error_log', '/path/to/php_errors.log');
+
+ob_start();
+
+require($_SERVER['DOCUMENT_ROOT'] . '/php-processes/utilities.php');
+dbConnect();
+
+
+$stmt = $conn -> prepare("UPDATE users SET bio=?, `fav-1`=?, `fav-2`=?, `fav-3`=? WHERE id=?");
+$stmt->bind_param("ssssi",
+                    $_POST["bio"],
+                   $_POST["fav-1"],
+                    $_POST["fav-2"],
+                    $_POST["fav-3"],
+                    $_SESSION["user_id"]);
+
+if ($stmt -> execute()) {
+        header("Location: /choose-pfp.html");
+        exit;
+    } else {
+        die("an unexpected error occured");
+}
+
+
+
+$stmt -> close();
+mysqli_close($conn);
+?>
