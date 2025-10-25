1813da088ecda4af6a523551b90a31db4db46652
Jamie <earthlove97@gmail.com>
2025-10-25 13:46:02 +0900

    echo

--- index.php
+++ index.php
@@ -1009,6 +1009,7 @@ if (isset($_SESSION["user_id"])) {
 // echo "Interval of: "."$intervals"." |  ";
 // echo "You have a streak of: "."$streak"." |  ";
 //* increase or reset streak count
+echo $startDate;
 if ($startDate !== "0000-00-00") {
     if ($started >= 0) {
         if ($intervals == 1) {
@@ -1031,7 +1032,6 @@ if ($startDate !== "0000-00-00") {
         }
     } 
 } else {
-    echo "I should not be here";
     if ($intervals == 1) {
         $streak = $streak + 1;
         $fire = "off";
