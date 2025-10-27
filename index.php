aa7766a6751ca583adf1c159cbe0a4c3c317f6dc
Jamie-Work <earthlove97@gmail.com>
2025-10-27 10:10:37 +0900

    test

--- index.php
+++ index.php
@@ -1063,11 +1063,11 @@ if ($displayGoalDate == "0000-00-00") {
 $update_date = date("Y-m-d");
 
 date_default_timezone_set("Asia/Tokyo");
-echo date_default_timezone_get();
+// echo date_default_timezone_get();
 
 // $timezone = date_default_timezone_get();
-// $date = date('m/d/Y h:i:s a', time());
-// echo $date;
+$date = date('m/d/Y h:i:s a', time());
+echo $date;
 // echo "Today's date: "."$update_date";
 //* Set Session Tokens
 $_SESSION["pfp"] = $pfp_set;
