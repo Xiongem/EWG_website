baefa341b775cc66d0bcb32fcbe7a81a0feae769
Jamie-Work <earthlove97@gmail.com>
2025-10-28 08:14:02 +0900

    echo

--- index.php
+++ index.php
@@ -25,6 +25,7 @@ if (isset($_SESSION["user_id"])) {
     if ($timezone) {
         $_SESSION["timezone"] = $timezone;
         date_default_timezone_set("$timezone");
+        echo $timezone;
     }
                 
     //* Sets user's pfp
