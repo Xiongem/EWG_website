a6b90804e475151dc23ca4e525267fc7c6a62f2f
Jamie-Work <earthlove97@gmail.com>
2025-11-06 12:01:09 +0900

    changed js for reloading page after activity now reloads after 10 minutes orno mouse over or keypress

--- index.php
+++ index.php
@@ -2405,58 +2405,23 @@ $default25 = "images/badges/cross-finish-mono.webp";
         }
     }
     //`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    //* refreshes page after 10 minutes of inactivity
-    // Refresh Rate is how often you want to refresh the page 
-    // bassed off the user inactivity. 
-    var refresh_rate = 600; //<-- In seconds, change to your needs
-    var last_user_action = 0;
-    var has_focus = false;
-    var lost_focus_count = 0;
-    // If the user loses focus on the browser to many times 
-    // we want to refresh anyway even if they are typing. 
-    // This is so we don't get the browser locked into 
-    // a state where the refresh never happens.    
-    var focus_margin = 10; 
+    let reloadTime = 600000; // 10 minutes in milliseconds
 
-    // Reset the Timer on users last action
-    function reset() {
-        last_user_action = 0;
-        console.log("Reset");
-    }
+    let reloadPage = function() {
+        location.reload();
+    };
 
-    function windowHasFocus() {
-        has_focus = true;
-    }
+    let reloadTimeout = setTimeout(reloadPage, reloadTime);
 
-    function windowLostFocus() {
-        has_focus = false;
-        lost_focus_count++;
-        console.log(lost_focus_count + " <~ Lost Focus");
-    }
+    document.addEventListener("mousemove", function() {
+        clearTimeout(reloadTimeout);
+        reloadTimeout = setTimeout(reloadPage, reloadTime);
+    });
 
-    // Count Down that executes ever second
-    setInterval(function () {
-        last_user_action++;
-        refreshCheck();
-    }, 1000);
-
-    // The code that checks if the window needs to reload
-    function refreshCheck() {
-        var focus = window.onfocus;
-        if ((last_user_action >= refresh_rate && !has_focus && document.readyState == "complete") || lost_focus_count > focus_margin) {
-            window.location.reload(); // If this is called no reset is needed
-            reset(); // We want to reset just to make sure the location reload is not called.
-        }
-
-    }
-    window.addEventListener("focus", windowHasFocus, false);
-    window.addEventListener("blur", windowLostFocus, false);
-    window.addEventListener("click", reset, false);
-    window.addEventListener("mousemove", reset, false);
-    window.addEventListener("keypress", reset, false);
-    window.addEventListener("scroll", reset, false);
-    document.addEventListener("touchMove", reset, false);
-    document.addEventListener("touchEnd", reset, false);
+    document.addEventListener("keypress", function() {
+        clearTimeout(reloadTimeout);
+        reloadTimeout = setTimeout(reloadPage, reloadTime);
+    });
     </script>
 </body>
 </html>
