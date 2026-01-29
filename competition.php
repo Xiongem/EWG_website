081558535e5155e6d4eb04e148a16faf80503de2
Jamie Work <earthlove97@gmail.com>
2026-01-29 16:09:13 +0900

    test

--- competition.php
+++ competition.php
@@ -50,7 +50,7 @@ $userID = htmlspecialchars($_SESSION["user_id"]);
                     </div> -->
                     <div class="option-wrapper">
                         <div class="radio-wrapper-9 radio">
-                            <input id="casual" type="radio" name="chooseLeague" value="casual">
+                            <input id="casual" type="radio" name="chooseLeague" value="casual" checked>
                             <label for="casual">Casual League</label>
                         </div>
                         <div class="radio-wrapper-9 radio">
